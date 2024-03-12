c034bc7aae42727fa64c220de6d0b81a5d4db0e5
SterrenHeer <sterrenheer@gmail.com>
2024-03-12 12:47:49 +0300

    delete tabs for items in promo section

--- js/script.js
+++ js/script.js
@@ -2,44 +2,6 @@ $('.header_toggler').click(function() {
     $('.header_contacts').toggleClass('show flex');
 });
 
-function tabs(tabContainerSelector, tabsSelector, tabsContentSelector, tabsParentSelector, activeClass) {
-    let tabsParent = document.querySelector(`${tabContainerSelector} ${tabsParentSelector}`),
-        tabs = document.querySelectorAll(`${tabContainerSelector} ${tabsSelector}`),
-		tabsContent = document.querySelectorAll(`${tabContainerSelector} ${tabsContentSelector}`);
-
-	function hideTabContent() {
-        tabsContent.forEach(item => {
-            item.classList.add('hide');
-            item.classList.remove('show');
-        });
-
-        tabs.forEach(item => {
-            item.classList.remove(activeClass);
-        });
-	}
-
-	function showTabContent(i = 0) {
-        tabsContent[i].classList.add('show');
-        tabsContent[i].classList.remove('hide');
-        tabs[i].classList.add(activeClass);
-    }
-    
-    hideTabContent();
-    showTabContent();
-
-	tabsParent.addEventListener('click', function(event) {
-		const target = event.target;
-		if(target && target.classList.contains(tabsSelector.slice(1))) {
-            tabs.forEach((item, i) => {
-                if (target == item) {
-                    hideTabContent();
-                    showTabContent(i);
-                }
-            });
-		}
-	});
-}
-
 function slider({container, wrapper, field, slide, indicatorsSelector, nextArrow, prevArrow}) {
     let slideIndex = 1,
         offset = 0,
@@ -189,11 +151,6 @@ function modal(triggerSelector, closeSelector, modalSelector) {
 
     window.addEventListener('scroll', showModalByScroll);
 }
-
-tabs('.promo_content_item.tab_1', '.promo_tab_item', '.promo_tab_content', '.promo_tab_items', 'promo_tab_active');
-tabs('.promo_content_item.tab_2', '.promo_tab_item', '.promo_tab_content', '.promo_tab_items', 'promo_tab_active');
-tabs('.promo_content_item.tab_3', '.promo_tab_item', '.promo_tab_content', '.promo_tab_items', 'promo_tab_active');
-tabs('.promo_content_item.tab_4', '.promo_tab_item', '.promo_tab_content', '.promo_tab_items', 'promo_tab_active');
 
 slider({
     container: '.gallery_slider',
